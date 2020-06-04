e2ab578be71d40cefe364f33158a8655846b3286
feedkeek <feedkeek@gmail.com>
2020-06-04 21:28:41 +0400

    Currency Added

--- forms/anbarForm/pages/logs/logs.js
+++ forms/anbarForm/pages/logs/logs.js
@@ -6,6 +6,8 @@ poolConnect.then((pool) => {
 	pool.request().execute("dbo.exec_all_logs", (err, res) => {
 		generateTable($("#logsTable")[0], res.recordset);
 		generateTableHead($("#logsTable")[0], ["Operation", "Action", "Date", "Username"]);
-		$("tbody").wrap("<div class='tbody' style='height:100%; width: calc(400% + 10px); overflow-y: scroll'></div>")
+		$("td").addClass("logsTableElement");
+		$("th").addClass("logsTableElement");
+		$("tbody").wrap("<div class='tbody' style='height:100%; width: calc(400% + 10px); overflow-y: scroll'></div>");
 	});
 });
