22dde4dd0f30aff0d021c2fba65b6d0a45ed9e62
Aghamoghlanov Rustam <raghamoghlanov@gmail.com>
2020-08-31 15:20:30 +0400

    Revert "Added ENG,RUS,AZE languages for data from tables(except anbarRemove).Temporary fix for data input in anbarAdd."
    This reverts commit 65a893f22580d430fbd8052861260345bcadf19e.

--- anbarForm/pages/sellers/sellers.js
+++ anbarForm/pages/sellers/sellers.js
@@ -1,54 +1,33 @@
-const { lang } = require("moment");
-
 var sellersSelectedId = -1;
-var procedures = [
-	"dbo.product_seller_select_active",
-	"dbo.product_seller_select_deactive",
-];
+var procedures = ["dbo.product_seller_select_active","dbo.product_seller_select_deactive"]
 var procedureId = 0;
 
-function fillTable(procedureId) {
+function fillTable(procedureId){
 	poolConnect.then((pool) => {
 		pool.request().execute(procedures[procedureId], (err, res) => {
-			data = [];
-			for (let i of res.recordset) {
+			data = []
+			for(let i of res.recordset){
 				data.push(i);
 				console.log(i);
 			}
 			$(".sellersProductTable").remove();
-
+						
 			// let head = ["Title", "Firstname", "Lastname", "Patronymic", "Active", "P Address", "Phone Number"];
-			$(".sellers-table-data").append(
-				"<table class='sellersProductTable'></table>"
-			);
+			$(".sellers-table-data").append("<table class='sellersProductTable'></table>");
 			$(".sellersProductTable").append("<thead></thead>");
 			$(".sellersProductTable").append("<tbody></tbody>");
-
-			$(".sellersProductTable > thead").append(
-				`<th>${languages["seller"]}:</th>`
-			);
-			$(".sellersProductTable > thead").append(
-				`<th>${languages["firstname"]}:</th>`
-			);
-			$(".sellersProductTable > thead").append(
-				`<th>${languages["secondname"]}:</th>`
-			);
-			$(".sellersProductTable > thead").append(
-				`<th>${languages["patronymic"]}:</th>`
-			);
-			$(".sellersProductTable > thead").append(
-				`<th>${languages["status"]}:</th>`
-			);
-			$(".sellersProductTable > thead").append(
-				`<th>${languages["address"]}:</th>`
-			);
-			$(".sellersProductTable > thead").append(
-				`<th>${languages["phonenumber"]}:</th>`
-			);
-
+	
+			$(".sellersProductTable > thead").append(`<th>Title:</th>`);
+			$(".sellersProductTable > thead").append(`<th>Firstname:</th>`);
+			$(".sellersProductTable > thead").append(`<th>Lastname:</th>`);
+			$(".sellersProductTable > thead").append(`<th>Patronymic:</th>`);
+			$(".sellersProductTable > thead").append(`<th>Active:</th>`);
+			$(".sellersProductTable > thead").append(`<th>P Address:</th>`);
+			$(".sellersProductTable > thead").append(`<th>Phone Number:</th>`);
+			
 			data.forEach((el) => {
 				let row = `<tr>`;
-				row += `<td style="display:none" id="sellersId">${el.id}</td>`;
+				row += `<td style="display:none" id="sellersId">${el.id}</td>`
 				row += `<td>${el.seller}</td>`;
 				row += `<td>${el.firstname}</td>`;
 				row += `<td>${el.lastname}</td>`;
@@ -56,7 +35,7 @@ function fillTable(procedureId) {
 				row += `<td>${el.active == 1 ? "Yes" : "No"}</td>`;
 				row += `<td>${el.p_address}</td>`;
 				row += `<td>${el.phonenumber}</td>`;
-
+	
 				row += "</tr>";
 				$(".sellersProductTable > tbody").append(row);
 			});
@@ -65,13 +44,11 @@ function fillTable(procedureId) {
 			let tr = document.querySelectorAll("tbody tr");
 			tr.forEach((el) => {
 				el.addEventListener("click", () => {
-					tr.forEach((trel) => {
-						trel.classList.remove("selected");
-					});
+					tr.forEach((trel) => {trel.classList.remove("selected")});
 					el.classList.toggle("selected");
 					sellersSelectedId = el.firstChild.textContent;
-				});
-			});
+				})
+			})
 		});
 	});
 }
@@ -84,7 +61,7 @@ $(".sellersInputButton").click(() => {
 
 $(".sellersPopupCloseButton").click(() => {
 	$(".sellersPopUpContainer").hide();
-});
+})
 
 $(".addSellersButton").on("click", () => {
 	let title = $("input#title").val();
@@ -93,74 +70,69 @@ $(".addSellersButton").on("click", () => {
 	let patronymic = $("input#patronymic").val();
 	let phoneNumber = $("input#phonenumber").val();
 	let p_address = $("input#paddress").val();
-	let active = $("input#active").val() == "1" ? 1 : 0;
-	if (
-		title.length == 0 ||
-		firstname.length == 0 ||
-		lastname.length == 0 ||
-		patronymic.length == 0 ||
-		p_address.length == 0
-	) {
-		alert("All fields must be filled");
-		return;
+	let active = $("input#active").val() == '1' ? 1 : 0;
+	if(title.length == 0 || firstname.length == 0
+		|| lastname.length == 0 || patronymic.length == 0
+    || p_address.length == 0){
+			alert("All fields must be filled");
+			return;
 	}
 	poolConnect.then((pool) => {
-		pool
-			.request()
-			.input("seller", mssql.NVarChar(250), title)
-			.input("firstname", mssql.NVarChar(250), firstname)
-			.input("lastname", mssql.NVarChar(250), lastname)
-			.input("patronymic", mssql.NVarChar(250), patronymic)
-			.input("user_id", mssql.Int, USER["id"])
-			.input("active", mssql.Int, active)
-			.input("phonenumber", mssql.NVarChar(250), phoneNumber)
-			.input("p_address", mssql.NVarChar(250), p_address)
-			.execute("dbo.product_sellers_insert", (err, res) => {
-				$("input").val("");
-				$("#active").prop("checked", false);
-				console.log(err);
-				fillTable(procedureId);
-			});
-	});
+		pool.request()
+				.input("seller", mssql.NVarChar(250), title)
+				.input("firstname", mssql.NVarChar(250), firstname)
+				.input("lastname", mssql.NVarChar(250), lastname)
+				.input("patronymic", mssql.NVarChar(250), patronymic)
+				.input("user_id", mssql.Int, USER["id"])
+				.input("active", mssql.Int, active)
+				.input("phonenumber", mssql.NVarChar(250), phoneNumber)
+				.input("p_address", mssql.NVarChar(250), p_address)
+				.execute("dbo.product_sellers_insert", (err, res)=>{
+					$("input").val("");
+					$("#active").prop("checked", false);
+					console.log(err);
+					fillTable(procedureId);
+				})
+	})
 });
 
+
 $("#sellerDelete").on("click", () => {
-	if (!sellersSelectedId) {
+	if(!sellersSelectedId){
 		alert("Please select row.");
 	}
 	poolConnect.then((pool) => {
-		pool
-			.request()
-			.input("id", mssql.Int, sellersSelectedId)
-			.input("user_id", mssql.Int, USER["id"])
-			.execute("product_sellers_delete", (err, res) => {
-				fillTable(procedureId);
-			});
-	});
-});
+		pool.request()
+				.input("id", mssql.Int, sellersSelectedId)
+				.input("user_id", mssql.Int, USER["id"])
+				.execute("product_sellers_delete", (err, res) => {
+					fillTable(procedureId);
+				})
+	})
+})
 
 $("#showContent").on("click", () => {
-	if (procedureId == 0) {
+	if(procedureId == 0){
 		procedureId = 1;
 		$("#showContent").text("Show Active");
-	} else {
+	}
+	else{
 		procedureId = 0;
 		$("#showContent").text("Show Inactive");
 	}
 	fillTable(procedureId);
-});
+})
 
 $("#changeStatus").on("click", () => {
-	if (!sellersSelectedId) alert("Please select a row");
+	if(!sellersSelectedId) alert("Please select a row");
 	poolConnect.then((pool) => {
-		pool
-			.request()
-			.input("id", mssql.Int, sellersSelectedId)
-			.input("user_id", mssql.Int, USER["id"])
-			.execute("dbo.product_sellers_deactivate_or_activate", (err, res) => {
-				console.log(err);
-				fillTable(procedureId);
-				sellersSelectedId = null;
-			});
-	});
-});
+		pool.request()
+				.input("id", mssql.Int, sellersSelectedId)
+				.input("user_id", mssql.Int, USER["id"])
+				.execute("dbo.product_sellers_deactivate_or_activate", (err, res) => {
+					console.log(err);
+					fillTable(procedureId);
+					sellersSelectedId = null;
+				})
+	})
+})
